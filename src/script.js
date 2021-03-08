4e975695101036b82f1847d322a71f82c7865a2b
Diego Maia <diego.maia@montreal.com.br>
2021-03-08 14:59:38 -0300

    Criação de funções

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-var nome = "Diego";
+//Função entrar
+function entrar(){
+    var area = document.getElementById('area');
+    var texto = prompt('Digite o seu nome: ');
+
+    if(texto == '' || texto == null){
+        alert('Digite o seu nome novamente!')
+        area.innerHTML = 'Bem vindo...';
+    }else{
+        area.innerHTML =  'Bem vindo ' + texto;
+    }
+}
+
+function entrar2(nome){
+    var area = document.getElementById('area2');
+    var texto = prompt('Digite o seu sobrenome: ');
+    area.innerHTML = nome + " " + texto;
+}
