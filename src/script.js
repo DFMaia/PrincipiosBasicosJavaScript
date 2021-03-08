37549a0837d8d036035ddbbbc5fcf3a437162ba7
Diego Maia <diego.maia@montreal.com.br>
2021-03-08 16:23:41 -0300

    Utilização do Switch e alteração de alguns arquivos README

--- src/script.js
+++ src/script.js
@@ -16,3 +16,46 @@ function entrar2(nome){
     var texto = prompt('Digite o seu sobrenome: ');
     area.innerHTML = nome + " " + texto;
 }
+
+/*
+//Sobre o while
+var x = 0;
+while(x < 10){
+    document.write("O valor de x é: " + x + "</br>");
+    x++;
+}
+
+//Sobre o for
+for(var a = 0; a < 10; a++){
+    document.write("O valor de a é: " + a + "</br>");
+    x++;
+}*/
+
+document.write("Escolha o seu pedido: </br>");
+document.write("</br> 0 - Sorvete / 1 - Suco </br>");
+document.write("</br> 2 - Coca-Cola / 3 - Água gelada </br></br>");
+
+function pedir(){
+    var x = prompt("O que deseja pedir?");
+    switch(x){
+
+        case "0":
+            alert("Você pediu  um sorvete;");
+            break;
+
+        case "1":
+            alert("Você pediu suco");
+            break;
+
+        case "2": 
+            alert("Você pediu  uma Coca-Cola");
+            break;
+
+        case "3": 
+        alert("Você pediu uma água gelada");
+        break;
+
+        default:
+            alert("Essa opção não existe.");    
+    }
+}
