4be433b304ac5da6b9c0e92f9d5b3fb0dd626dbb
Diego Maia <diego.maia@montreal.com.br>
2021-03-09 14:33:01 -0300

    Exemplo no código com comentário sobre funções anônimas

--- src/script.js
+++ src/script.js
@@ -1,22 +1,39 @@
-//Para mapear cada um dos index e somar com o valor do index
-const lista = [1,2,3,4,5,6,];
+/**
+ * FUNÇÃO ANÔNIMA
+ * A função reduce que se encontra dentro da função adicionar, é uma função anônima.
+ */
+function adicionar(...numeros){
+    let total = numeros.reduce(function(total, proximo){
+        return total + proximo
+    });
 
-const novaLista = lista.map(function(item, index) {
-   return index + item; 
-});
+    console.log(total);
+}
 
-console.log(novaLista);
+adicionar(1,2,3,4,5);
 
-//Reduce vai somar com o index atual mais o próximo.
-const soma = lista.reduce(function(total, proximo){
-    return total + proximo;
-});
+/**
+ * Esse exemplo aqui é o mesmo que o de disse. 
+ * A diferença é que ele é uma versão mais simplificada. 
+ * Substitui a palavra function por =>
+ */
+ function adicionar(...numeros){
+    let total = numeros.reduce((total, proximo) => {
+        return total + proximo
+    });
 
-console.log(soma);
+    console.log(total);
+}
 
-//Encontrando um número na lista. 
-const find = lista.find(function(item){
-    return item === 6;
-});
+adicionar(1,2,3,4,5);
 
-console.log(find);
+/**
+ * E essa versão aqui é mais reduzida ainda. 
+ */
+ function adicionar(...numeros){
+    let total = numeros.reduce((total, proximo) => total + proximo);
+
+    console.log(total);
+}
+
+adicionar(1,2,3,4,5);
