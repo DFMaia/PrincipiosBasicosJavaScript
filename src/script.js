ad6b465138f9f8fac86ba0d2eb0c0a9509c7e21e
Diego Maia <diego.maia@montreal.com.br>
2021-03-09 14:22:25 -0300

    Exemplo em código de encontrar elementos na lista e manipulação matemáticas de valores dentro da lista.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
-function cadastrar(usuarios, ...novosUsuarios) {
-    let totalUsuarios = [
-        ...usuarios,
-        ...novosUsuarios
-    ];
+//Para mapear cada um dos index e somar com o valor do index
+const lista = [1,2,3,4,5,6,];
 
-    return console.log(totalUsuarios);
-}
+const novaLista = lista.map(function(item, index) {
+   return index + item; 
+});
 
-let usuarios = ["Matheus", "João"];
+console.log(novaLista);
 
-let novosUsuarios = cadastrar(usuarios, "Henrique", "Lucas");
+//Reduce vai somar com o index atual mais o próximo.
+const soma = lista.reduce(function(total, proximo){
+    return total + proximo;
+});
+
+console.log(soma);
+
+//Encontrando um número na lista. 
+const find = lista.find(function(item){
+    return item === 6;
+});
+
+console.log(find);
